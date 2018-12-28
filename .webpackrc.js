e86d5df41637046f9d31c56defa87154c00137f1
郭晋岱 <guojindai@zccp.com>
2018-12-28 15:55:48 +0800

    dist 的文件带 hash

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,5 +1,10 @@
 export default {
   "extraBabelPlugins": [
     ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }]
-  ]
+  ],
+  "html": {
+    "template": "./src/index.ejs",
+    "inject": true
+  },
+  "hash": true
 }
